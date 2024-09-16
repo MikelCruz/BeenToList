17693b261d62ce660597c8e5dd8bc57b7a5468fe
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-09-16 18:58:58 +0200

    [UPDATE] Added new state beenListPage && Updated goBack button for the new component BeenList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import { BeenItem } from './components/BeenItem.jsx'
+import { BeenList} from './components/BeenList.jsx'
 import './App.css'
 
 function App() {
 
-  const [beenAddPage, setBeenAddPage] = useState(false);
+  const [beenAddPage,   setBeenAddPage]   = useState(false);
+  const [beenListPage,  setBeenListPage]  = useState(false);
+
 
   useEffect(() => {
     
@@ -12,15 +15,17 @@ function App() {
 
   return (
     <main>
-      {beenAddPage && <BeenItem  goBack={() => setBeenAddPage(false)} />}
+      {beenAddPage  && <BeenItem  goBack={() => setBeenAddPage(false)} />}
+
+      {beenListPage && <BeenList  goBack={() => setBeenListPage(false)} />}
       
-      {!beenAddPage && 
+      {!beenAddPage && !beenListPage && 
       <>
         <h1>Been To list</h1>
 
         <button onClick={()=> ( setBeenAddPage(true))} className="btn-add-places"> Agregar Lugares </button>
         
-        <button className="btn-show-places"> Mostrar Lista</button>
+        <button onClick={()=> ( setBeenListPage(true))} className="btn-show-places"> Mostrar Lista</button>
 
         <footer> By Mikel Cruz</footer>
       </>
