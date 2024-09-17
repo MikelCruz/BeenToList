9185517e7b10e3a6774594d3c4e48f9571451a14
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-09-17 13:49:20 +0200

    [REFACTOR] Change the useState variables for one state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react';
 import { BeenAddItem } from './components/BeenAddItem.jsx'
-import { BeenList} from './components/BeenList.jsx'
+import { BeenList } from './components/BeenList.jsx'
 import './App.css'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
 
-  const [beenAddPage,   setBeenAddPage]   = useState(false);
-  const [beenListPage,  setBeenListPage]  = useState(false);
-
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <main>
-      {beenAddPage  && <BeenAddItem  goBack={() => setBeenAddPage(false)} />}
-
-      {beenListPage && <BeenList  goBack={() => setBeenListPage(false)} />}
-      
-      {!beenAddPage && !beenListPage && 
-      <>
-        <h1>Been To list</h1>
-
-        <button onClick={()=> ( setBeenAddPage(true))} className="btn-add-places"> Agregar Lugares </button>
-        
-        <button onClick={()=> ( setBeenListPage(true))} className="btn-show-places"> Mostrar Lista</button>
-
-        <footer> By Mikel Cruz</footer>
-      </>
-      }
+      {currentPage === 'add' && <BeenAddItem goBack={() => handlePageChange('home')} />}
+      {currentPage === 'list' && <BeenList goBack={() => handlePageChange('home')} />}
+      {currentPage === 'home' && (
+        <>
+          <h1>Been To list</h1>
+          <button onClick={() => handlePageChange('add')} className="btn-add-places">Agregar Lugares</button>
+          <button onClick={() => handlePageChange('list')} className="btn-show-places">Mostrar Lista</button>
+          <footer> By Mikel Cruz</footer>
+        </>
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
