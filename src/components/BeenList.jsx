b16b6549144c715719c3787b65bbf9fd2beb6f5d
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-09-17 13:54:28 +0200

    [REFACTOR] Deleted the handleGoBack function and use the linter for clean code

--- src/components/BeenList.jsx
+++ src/components/BeenList.jsx
@@ -1,25 +1,17 @@
 import { useSelector } from "react-redux";
 import { IoChevronBackCircleOutline } from "react-icons/io5";
-
 import { BeenItem } from "./BeenItem";
 
 // eslint-disable-next-line react/prop-types
 export const BeenList = ({ goBack }) => {
-  
   const places = useSelector((state) => state.place.places);
-
-  const handleGoBack = () => {
-    goBack();
-  };
 
   return (
     <main>
-      <h2> Lista de Lugares </h2>
-      <IoChevronBackCircleOutline className="icon-go-back" onClick={handleGoBack} />
-  
+      <h2>Lista de Lugares</h2>
+      <IoChevronBackCircleOutline className="icon-go-back" onClick={goBack} />
       <div className="places-list">
         {places.length === 0 && <h2>¡Aun no has visitado ningún lugar!</h2>}
-  
         {places.map((place, index) => (
           <BeenItem place={place} index={index} key={index} />
         ))}
